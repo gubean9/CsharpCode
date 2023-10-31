786eb04dbb2adfb3d1020adb1091d4e927935c96
SubeanNo <127818787+gubean9@users.noreply.github.com>
2023-10-31 16:31:59 +0900

    Var의 사용과 타입 출력

---
+++ Day002/11.UsingVar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingVar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 20;
+            Console.WriteLine("Type:{0}, Value:{1}", a.GetType(), a); // a의 타입과 값을 각각 출력
+            Console.WriteLine($"Type:{a.GetType()}, Value:{a}");
+        }
+    }
+}
