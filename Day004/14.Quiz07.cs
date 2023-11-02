4e559a63b28b319a4e45c1c67a34a2aaa2a83c93
SubeanNo <127818787+gubean9@users.noreply.github.com>
2023-11-02 15:23:54 +0900

    Q7 : 232p 예제 / 평균 구하는 프로그램 완성하기
    코드 수정으로 원하는 결과값 구하기

---
+++ Day004/14.Quiz07.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz07
+{
+    internal class Program
+    {
+        /* 원본 코드
+        static void Main(string[] args)
+        {
+            double mean = 0;
+
+            Mean(ref 1, 2, 3, 4, 5, mean);
+            Console.WriteLine($"평균 : {mean}");
+        }
+
+        public static void Mean(ref double a, ref double b, ref double c, ref double d, ref double e, double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+        */
+
+        // 수정한 코드
+        static void Main(string[] args)
+        {
+            double mean = 0;
+            double a = 1, b = 2, c = 3, d = 4, e = 5;
+
+            Mean(a, b, c, d, e, ref mean);
+            Console.WriteLine($"평균 : {mean}");
+        }
+
+        public static void Mean(double a, double b, double c, double d, double e, ref double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
